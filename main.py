fe6cbc60fe725e2c14382b17c8b7abf28ca35c11
Edridge D'Souza <edridge@gmail.com>
2022-01-07 17:48:08 -0500

    Credit

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3
 
+# Data from INN Stembook 2018
+# https://www.who.int/publications/i/item/who-emp-rht-tsn-2018-1
+# https://cdn.who.int/media/docs/default-source/international-nonproprietary-names-(inn)/stembook-2018.pdf?sfvrsn=32a51b3c_6&download=true
