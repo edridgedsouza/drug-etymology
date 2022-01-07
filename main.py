dee38c66f2d8797b9adfca12b15d1f77fa223f9e
Edridge D'Souza <edridge@gmail.com>
2022-01-07 18:32:26 -0500

    Attempt py2js

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 # https://www.who.int/publications/i/item/who-emp-rht-tsn-2018-1
 # https://cdn.who.int/media/docs/default-source/international-nonproprietary-names-(inn)/stembook-2018.pdf?sfvrsn=32a51b3c_6&download=true
 
+from pscript import py2js
 import re
+
 
 class Linguist():
 
@@ -43,13 +45,17 @@ class Linguist():
         for stem, pattern in self.patterns.items():
             if re.search(pattern, drug):
                 matching_roots.append(stem)
+
+        out = {}  # py2js can't handle a dict comp
+        for stem in matching_roots:
+            out[stem] = self.definitions[stem]
                 
-        return {stem:self.definitions[stem] for stem in matching_roots}
+        return out
 
     def explain(self, drug):
         res = self._etymology(drug)
         if res:
-            lst = '\n'.join([f'{stem}:\t{defn}' for stem, defn in res.items()])
+            lst = '\n'.join([f'\t{stem}:\t{defn}' for stem, defn in res.items()])
             out = (f'Possible etymologies for drug {drug}:\n{lst}\n')
         else:
             out = (f'No matching etymologies for drug {drug}.\n')
@@ -58,10 +64,15 @@ class Linguist():
 
 if __name__ == '__main__':
     l = Linguist('./stems.tsv')
+
+    print(py2js(Linguist))
+   
+    """
     while True:
         d = input('What drug would you like to analyze?\n')
         if d.lower() == 'exit':
             print('Thanks for using my tool! Check it out on GitHub '
-                  '@edridgedsouza/drug-etymology')
+                  'https://github.com/edridgedsouza/drug-etymology')
             break
         print(l.explain(d))
+    """
